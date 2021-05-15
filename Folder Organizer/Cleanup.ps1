78e2020b0ec80789b9790c9ce0842ae3ab5acfb6
Lucas Eduardo Hoeltgebaum <69653148+Lucas-HGB@users.noreply.github.com>
2021-05-15 11:54:50 -0300

    Add files via upload

---
+++ Folder Organizer/Cleanup.ps1
@@ -0,0 +1,18 @@
+$Folders = "C:\Battlestate Games\EFT\EscapeFromTarkov_Data", "C:\Battlestate Games\BsgLauncher\Temp", "S:\Documents\Default.rdp", "S:\Pictures\Camera Roll", "S:\Pictures\Saved Pictures", "S:\Videos\AnyDesk", "S:\Videos\Captures", "C:\Users\lucas\AppData\Local\Microsoft\Outlook\lucas.hgb2@gmail.com.ost", "C:\Users\lucas\AppData\Local\Microsoft\Outlook\lehoeltgebaum@furb.br.ost", "C:\Users\lucas\AppData\Local\Microsoft\Outlook\lucas.hoeltgebaum@workdb.com.br.nst", "C:\Users\lucas\AppData\Local\Microsoft\Edge", "C:\hiberfil.sys",
+"C:\Users\lucas\AppData\Local\Mozilla\Firefox\Profiles\mrcax662.default-release\cache2\entries", "C:\Users\lucas\AppData\Local\Temp", "C:\Windows\Temp",
+"C:\Users\lucas\AppData\Local\Microsoft\Outlook\lehoeltgebaum@furb.br.nst", "C:\Users\lucas\AppData\Roaming\Slack\Service Worker\CacheStorage", "C:\Users\lucas\AppData\Roaming\Slack\Cache", "C:\Users\lucas\AppData\Roaming\Code\Cache", "C:\Users\lucas\AppData\Roaming\Code\CachedData", "C:\Users\lucas\AppData\Roaming\Microsoft\Teams\Service Worker\CacheStorage", "C:\Windows\Downloaded Program Files", "C:\Windows\SoftwareDistribution\Download",
+"C:\Users\lucas\AppData\Local\Microsoft\Windows\Explorer",
+"C:\$Recycle.Bin\S-1-5-21-160320902-3691717267-1884631123-1001"
+
+$totalSize = 0
+$folderSize = 0
+$folderCount = 0
+foreach ($Folder in $Folders) {if (Test-Path -Path $Folder) {
+	$folderSize = "{0:N2}" -f ((gci -force -Recurse -erroraction 'silentlycontinue' "$Folder" | measure Length -erroraction 'silentlycontinue' -s).sum / 1Gb) 
+	$totalSize += $folderSize
+	$folderCount += 1
+	#Remove-Item -Recurse -Force -Path "$Folder" -erroraction 'silentlycontinue'
+}}
+
+
+powershell -ExecutionPolicy bypass -command "& { . S:\Documents\Programs\Notify.ps1; Notify -Title 'System Cleanup' -PathToImg '$PSScriptRoot\BroomIcon.png' -Text 'Removed $totalSize GB from $folderCount Folders' }"
---
+++ Folder Organizer/Notify.ps1
@@ -0,0 +1,143 @@
+function Notify($Text, $Title, $Base64, $Action, $PathToImg)	
+{
+	# Demo script to display a custom 'toast' notification
+
+	# Load required assemblies
+	Add-Type -AssemblyName PresentationFramework, System.Windows.Forms
+
+	# User-populated variables
+	$WindowHeight = 100
+	$WindowWidth = 400
+	$Timeout = 10
+	$ImageHeight = 80
+	$ImageWidth = 80
+	if ($PathToImg) {
+		$Base64 = [convert]::ToBase64String((get-content $PathToImg -encoding byte))
+	}
+
+	# Set screen working area, bounds and start and finish location of the window 'top' property (for animation)
+	$workingArea = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea
+	$Bounds = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
+	$TopStart = $workingArea.Bottom
+	$TopFinish = $workingArea.Bottom - ($WindowHeight + 10)
+	$CloseFinish = $Bounds.Bottom
+
+	# Create the custom logo from Base64 string
+	$CustomImage = New-Object System.Windows.Media.Imaging.BitmapImage
+	$CustomImage.BeginInit()
+	$CustomImage.StreamSource = [System.IO.MemoryStream][System.Convert]::FromBase64String($Base64)
+	$CustomImage.EndInit()
+
+	# Calculate element dimensions
+	$MainStackWidth = $WindowWidth - 10
+	$SecondStackWidth = $WindowWidth - $ImageWidth -10
+	$TextBoxWidth = $SecondStackWidth - 30
+
+	# Define the notification UI in Xaml
+	[XML]$Xaml = "
+	<Window
+	    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+	    xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'
+	    Title='Druva Notification' Width='$WindowWidth' Height='$WindowHeight'
+	    WindowStyle='None' AllowsTransparency='True' Background='Transparent' Topmost='True' Opacity='0.9'>
+	    <Window.Resources>
+	        <Storyboard x:Name='ClosingAnimation' x:Key='ClosingAnimation' >
+	            <DoubleAnimation Duration='0:0:.5' Storyboard.TargetProperty='Top' From='$TopFinish' To='$CloseFinish' AccelerationRatio='.1'/>
+	        </Storyboard>
+	    </Window.Resources>
+	    <Window.Triggers>
+	        <EventTrigger RoutedEvent='Window.Loaded'>
+	            <BeginStoryboard>
+	                <Storyboard >
+	                    <DoubleAnimation Duration='0:0:.5' Storyboard.TargetProperty='Top' From='$TopStart' To='$TopFinish' AccelerationRatio='.1'/>
+	                </Storyboard>
+	            </BeginStoryboard>
+	        </EventTrigger>
+	    </Window.Triggers>
+	    <Grid>
+	    <Border BorderThickness='0' Background='#333333'>
+	      <StackPanel Margin='20,10,20,10' Orientation='Horizontal' Width='$MainStackWidth'>
+	        <Image x:Name='Logo' Width='$ImageWidth' Height='$ImageHeight'/>
+	        <StackPanel Width='$SecondStackWidth'>
+	            <TextBox Margin='5' MaxWidth='$TextBoxWidth' Background='#333333' BorderThickness='0' IsReadOnly='True' Foreground='White' FontSize='20' Text='$Title' FontWeight='Bold' HorizontalContentAlignment='Center' Width='Auto' HorizontalAlignment='Stretch' IsHitTestVisible='False'/>
+	            <TextBox Margin='5' MaxWidth='$TextBoxWidth' Background='#333333' BorderThickness='0' IsReadOnly='True' Foreground='LightGray' FontSize='16' Text='$Text' HorizontalContentAlignment='Left' TextWrapping='Wrap' IsHitTestVisible='False'/>
+	        </StackPanel>
+	      </StackPanel>
+	    </Border>
+	  </Grid>
+	</Window>
+	"
+
+	# Create a global hash table to add dispatcher to
+	$Global:UI = @{}
+
+	# Create the window
+	$Window = [Windows.Markup.XamlReader]::Load((New-Object -TypeName System.Xml.XmlNodeReader -ArgumentList $xaml))
+
+	# Set the image
+	$Logo = $Window.FindName('Logo')
+	$Logo.Source = $CustomImage
+
+	# Add the closing animation to the global variable
+	$UI.ClosingAnimation = $Window.FindName('ClosingAnimation')
+
+	# Window loaded
+	$Window.Add_Loaded({
+
+	    # Activate
+	    $This.Activate()
+	    
+	    # Play a sound
+	    $SoundFile = "$env:SystemDrive\Windows\Media\Windows Notify.wav"
+	    $SoundPlayer = New-Object System.Media.SoundPlayer -ArgumentList $SoundFile
+	    $SoundPlayer.Add_LoadCompleted({
+	        $This.Play()
+	        $This.Dispose()
+	    })
+	    $SoundPlayer.LoadAsync()
+
+	    # Set the location of the left property
+	    $workingArea = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea
+	    $this.Left = $workingarea.Width - ($this.ActualWidth + 10)
+
+	    # Create a dispatcher timer to begin notification closure after x seconds
+	    $UI.DispatcherTimer = New-Object -TypeName System.Windows.Threading.DispatcherTimer
+	    $UI.DispatcherTimer.Interval = [TimeSpan]::FromSeconds($Timeout)
+	    $UI.DispatcherTimer.Add_Tick({
+	        $UI.ClosingAnimation.Begin($Window)
+	    })
+	    $UI.DispatcherTimer.Start()
+
+	})
+
+	# Window closing
+	$Window.Add_Closing({
+	    # Stop the dispatcher timer
+	    $UI.DispatcherTimer.Stop()
+	})
+
+	# Closing animation is completed
+	$UI.ClosingAnimation.Add_Completed({
+	    $Window.Close()
+	})
+
+	# Window Mouse enter
+	$Window.Add_MouseEnter({
+	    # Change cursor to a hand
+	    $This.Cursor = 'Hand'
+	})
+
+	# Window mouse up (simulate click)
+	$Window.Add_MouseUp({
+	    # Open a web page, stop the dispatcher timer and close the notification
+	    Start-Process "$Action"
+	    $UI.DispatcherTimer.Stop()
+	    $This.Close()
+	})
+
+	# Display the notification
+	$null = $window.Dispatcher.InvokeAsync{$window.ShowDialog()}.Wait()
+}
+
+
+#Notify "Removidos 5 GB de 23 Folders" "System Cleanup" "$IMG" "ACTION"
