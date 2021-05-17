7d848fda4a91bbaa68a50726339e9852d4bd4947
Lucas Eduardo Hoeltgebaum <69653148+Lucas-HGB@users.noreply.github.com>
2021-05-17 19:37:53 -0300

    Update PaiInstall.ps1

--- PaiInstall.ps1
+++ PaiInstall.ps1
@@ -3,13 +3,12 @@
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
 Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/Lucas-HGB/Dotfiles/main/Pai.py" -OutFile "$DesktopPath\MonitorarImagem.py"
 Invoke-WebRequest "https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe" -OutFile "$DesktopPath\Python3.exe"
-Write-Host "Após instalar o python com as definições padrões, aperte ENTER nessa tela do Powershell"
-Read-Host
+
+$Nothing = Read-Host -Prompt "Após instalar o python com as definições padrões, aperte ENTER nessa tela do Powershell"
 
 & "$DesktopPath\Python3.exe"
 
 $AppData = $env:LOCALAPPDATA
 & "$AppData\Programs\Python\Python39\python.exe -m pip install pillow"
 
-Write-Host "Para rodar o programa, clique 2x no arquivo 'MonitorarImagem.py', localizado em seu Desktop (:"
-Read-Host
+$Nothing = Read-Host -Prompt "Para rodar o programa, clique 2x no arquivo 'MonitorarImagem.py', localizado em seu Desktop (:"
