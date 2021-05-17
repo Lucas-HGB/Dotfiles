966508edb52d287815a82978eba86bfe1e8e7382
Lucas Eduardo Hoeltgebaum <69653148+Lucas-HGB@users.noreply.github.com>
2021-05-17 19:35:05 -0300

    Update PaiInstall.ps1

--- PaiInstall.ps1
+++ PaiInstall.ps1
@@ -4,6 +4,7 @@ $DesktopPath = [Environment]::GetFolderPath("Desktop")
 Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/Lucas-HGB/Dotfiles/main/Pai.py" -OutFile "$DesktopPath\MonitorarImagem.py"
 Invoke-WebRequest "https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe" -OutFile "$DesktopPath\Python3.exe"
 Write-Host "Após instalar o python com as definições padrões, aperte ENTER nessa tela do Powershell"
+Read Nothing
 
 & "$DesktopPath\Python3.exe"
 
