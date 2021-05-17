3fd15a81009c3e15537d38dc418782187799269f
Lucas Eduardo Hoeltgebaum <69653148+Lucas-HGB@users.noreply.github.com>
2021-05-17 20:01:41 -0300

    Delete PaiInstallpt2.ps1

--- PaiInstallpt2.ps1
+++
@@ -1,4 +0,0 @@
-$AppData = $env:LOCALAPPDATA
-& "$AppData\Programs\Python\Python39\python.exe -m pip install pillow"
-
-$Nothing = Read-Host -Prompt "Para rodar o programa, clique 2x no arquivo 'MonitorarImagem.py', localizado em seu Desktop (:"
