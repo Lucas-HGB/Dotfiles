dbf962abccaa2b8528eb0422d6c93f5d2eb88fc3
Lucas Eduardo Hoeltgebaum <69653148+Lucas-HGB@users.noreply.github.com>
2021-05-17 19:31:00 -0300

    Create PaiInstall.ps1

---
+++ PaiInstall.ps1
@@ -0,0 +1,10 @@
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/Lucas-HGB/Dotfiles/main/Pai.py" -OutFile "$DesktopPath\MonitorarImagem.py"
+Invoke-WebRequest "https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe" -OutFile "$DesktopPath\Python3.exe"
+& "$DesktopPath\Python3.exe"
+
+$AppData = $env:LOCALAPPDATA
+& "$AppData\Programs\Python\Python39\python.exe -m pip install pillow"
+
+Write-Host "Para rodar o programa, clique 2x no arquivo 'MonitorarImagem.py', localizado em seu Desktop (:"
+read Nothing
