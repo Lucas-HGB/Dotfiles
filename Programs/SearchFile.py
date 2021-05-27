2597c514cd3f4ae20b89f1f6f24c5d5fb1d40189
Lucas Eduardo Hoeltgebaum <69653148+Lucas-HGB@users.noreply.github.com>
2021-05-26 22:23:38 -0300

    Added folder exclusions.

--- Programs/SearchFile.py
+++ Programs/SearchFile.py
@@ -1,27 +1,43 @@
+#!/usr/bin/env python3
 from os import remove, listdir
-from os.path import isfile, join
+from os.path import isdir, join
+from argparse import ArgumentParser
 
-count = 0
-found = []
-def recurseFolder(folderToSearch, fileToSearch):
+def get_args():
+	parser = ArgumentParser()
+	parser.add_argument("-s", "--search", dest="search",
+						help="search", metavar="search")
+	parser.add_argument("-f", "--file", dest="file",
+						help="file", metavar="file")
+	parser.add_argument("-e", "--exclude", dest="exclude",
+					help="exclude", metavar="exclude", default="")
+	args = parser.parse_args()
+	return args
+
+def recurseFolder(folderToSearch, fileToSearch, exclusion = []):
 	global count
 	count += 1
-	print(f"Recursing over {folderToSearch}")
+	print(folderToSearch)
 	try:
-		for FileOrFolder in listdir(folderToSearch):
-			if fileToSearch in FileOrFolder:
-				found.append(f"Found File on {folderToSearch}\\{FileOrFolder}")
+		for FileOrFolder in [f for f in listdir(folderToSearch) if f.lower() not in exclusion]:
+			if fileToSearch.lower() in FileOrFolder.lower():
+				found.append(f"Found '{FileOrFolder}' at folder '{folderToSearch}'")
 
-			elif not isfile(join(folderToSearch, FileOrFolder)):
-				recurseFolder(join(folderToSearch,FileOrFolder), fileToSearch)
+			elif isdir(join(folderToSearch, FileOrFolder)):
+				recurseFolder(join(folderToSearch,FileOrFolder), fileToSearch, exclusion)
 	except PermissionError:
 		pass
-			
-folderToSearch = input("Insert folder to search\n")
-fileToSearch = input("Insert file to find\n")
 
-recurseFolder(folderToSearch=folderToSearch, fileToSearch=fileToSearch)
 
-print(f"\n\nRecursed over {count} folders!")
-for f in found:
-	print(f)
+
+if __name__ == "__main__":
+	count = 0
+	found = []
+	args = get_args()
+	recurseFolder(folderToSearch=args.search, fileToSearch=args.file, exclusion=["documents and settings", "winsxs", "servicing", "application data", "$recycle.bin", "sys", "proc", args.exclude.lower()])
+	print(f"\n\nRecursed over {count} folders and found {len(found)} appearances of the word '{args.file}'")
+	for f in found:
+		print(f)
+
+
+
