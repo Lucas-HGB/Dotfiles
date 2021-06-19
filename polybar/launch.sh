8f0b7aca68227ea6839678f0240af4c318b8a23a
Lucas Eduardo Hoeltgebaum <69653148+Lucas-HGB@users.noreply.github.com>
2021-06-18 22:51:03 -0300

    Choose polybar theme randomly

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-STYLE="transparent"
+STYLES_LIST=("transparent" "nordic" "angles")
+
+style="nordic" ## Only used if, for some reason, function below doesn't work
+
+function random_pick_from_list() {
+	len_list=${#STYLES_LIST[@]}
+	num=$(( $RANDOM % $len_list ))
+	style=${STYLES_LIST[$num]}
+}
+
+
+random_pick_from_list
+
 
 # Terminate already running bar instances
 killall -q polybar
@@ -9,5 +21,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-polybar -q left_monitor -c "$HOME"/.config/polybar/$STYLE/config.ini &
-polybar -q right_monitor -c "$HOME"/.config/polybar/$STYLE/config.ini & 
+polybar -q left_monitor -c "$HOME"/.config/polybar/$style/config.ini &
+polybar -q right_monitor -c "$HOME"/.config/polybar/$style/config.ini & 
