14af0603e4c67593b3d3d8f79a27eee3769cd7d9
Lucas Eduardo Hoeltgebaum <69653148+Lucas-HGB@users.noreply.github.com>
2021-08-10 20:09:39 -0300

    Delete CreateService.ps1

--- Programs/CreateService.ps1
+++
@@ -1,9 +0,0 @@
-$params = @{
-  Name = "KeyboardShortcuts"
-  BinaryPathName = 'S:\Documents\Programs\KeyboardShortcuts.exe'
-  DisplayName = "Keyboard shortcuts"
-  StartupType = "Automatic"
-  Description = "Script Python que controla diversos shortcuts; autocompletes; e funções do teclado"
-}
-
-New-Service @params
