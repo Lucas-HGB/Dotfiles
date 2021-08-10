a485280f2bec1d352987e24f6e834d855f855277
Lucas Eduardo Hoeltgebaum <69653148+Lucas-HGB@users.noreply.github.com>
2021-08-10 20:05:00 -0300

    Create Disable_Windows_Hotkeys.ps1

---
+++ Programs/src/Disable_Windows_Hotkeys.ps1
@@ -0,0 +1,9 @@
+Windows Registry Editor Version 5.00
+
+; Created by: Shawn Brink
+; http://www.sevenforums.com
+; Tutorial: http://www.sevenforums.com/tutorials/5937-windows-key-shortcuts-enable-disable.html
+
+
+[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer]
+"NoWinKeys"=dword:00000001
